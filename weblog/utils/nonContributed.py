25685f2639e6fa7c0fc219ee52ed528d15deef63
Vincent Liok <vliok@stuy.edu>
2016-11-01 20:07:23 -0400

    worked on displaying titles

--- weblog/utils/nonContributed.py
+++
@@ -1,9 +0,0 @@
-import csv
-import sqlite3
-
-f="../data/tables.db"
-db=sqlite3.connect(f)
-c=db.cursor()
-
-#make a list of the storyIDs in updates that the user contributed to
-q = "SELECT storyID FROM updates WHERE updates.userID = " + 
